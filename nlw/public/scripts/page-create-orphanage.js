48560637a431bc21d2183d3ca2f9812410ea6551
Verena Sousa <verena.ads19@hotmail.com>
2021-06-19 21:11:58 -0300

    commit

--- nlw/public/scripts/page-create-orphanage.js
+++ nlw/public/scripts/page-create-orphanage.js
@@ -18,17 +18,56 @@ let marker;
 
 // create and add marker
 map.on('click', (event) =>{
-    const lat = event.latlng.lat
+    const lat = event.latlng.lat;
     const lng = event.latlng.lng;
 
     document.querySelector("[name=lat]").value = lat;
     document.querySelector("[name=lng]").value = lng;
 
     //remove icon
-    marker && map.removeLayer(marker)
+    marker && map.removeLayer(marker);
 
     // add icon layer
     marker = L.marker([lat, lng], {icon})
     .addTo(map);
 
 })
+
+// adicionar o campo fotos
+function addPhotoField(){
+    // pegar o container de fotos #images
+    const container = document.querySelector('#images');
+
+    // pegar o container para duplicar .new-image
+    const fieldsContainer = document.querySelectorAll('.new-upload');
+    
+    // realizar o clone da última imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+
+    // verificar se o campo está vazio, se sim, não adiciionar ao container de imagens
+    const input = newFieldContainer.children[0];
+
+    if(input.value == ""){
+        return;
+    }
+    
+    // limpar o campo antes de adicionar ao container de imagens
+    input.value = "";
+        
+    // adicionar o clone ao container de #images 
+    container.appendChild(newFieldContainer);
+}
+
+function deleteField(event){
+    const span = event.currentTarget;
+
+    const fieldsContainer = document.querySelectorAll(".new-upload");
+
+    if (fieldsContainer.length <= 1){
+        // limpar o valor do campo
+        span.parentNode.children[0].value = "";
+        return;
+    }
+    // deletar o campo
+    span.parentNode.remove();
+}
