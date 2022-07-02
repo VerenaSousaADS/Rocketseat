b5006c5f17e9ba7f156bf55d3fd6e245aef803e8
Verena Sousa <verena.ads19@hotmail.com>
2022-07-02 16:08:43 -0300

    Criando rotas no servidor

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log("Olá Mundo!")
+//importar dependencias
+
+const express = require('express');
+const path = require('path');
+
+
+//iniciando o express
+const server = express()
+
+server
+
+    //utilizando os arquivos estáticos
+    .use(express.static('public'))
+
+    // criar uma rota 
+
+    .get('/', (request, response) => {
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+
+    })
+
+//ligar o servidor 
+
+server.listen(5500)
