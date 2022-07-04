c27f48dd6777fe7c7f9486b28f57b014d359288c
Verena Sousa <verena.ads19@hotmail.com>
2022-07-04 10:19:29 -0300

    Corrigindo erro de cach

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -29,7 +29,7 @@ const popup = L.popup({
     className: "map-popup",
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="./orphanage" class="choose-orphanage"> <img src="/images/arrow-white.svg"></a>')
+}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"></a>')
 
 //create and add marker
 
