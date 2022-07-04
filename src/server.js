8b446e78a8b7f61d97e49fb8c7c55fbe9bee0390
Verena Sousa <verena.ads19@hotmail.com>
2022-07-04 09:21:30 -0300

    Transformando html em uma engine hbs

--- src/server.js
+++ src/server.js
@@ -12,10 +12,15 @@ server
     //utilizando os arquivos estáticos
     .use(express.static('public'))
 
+    //configurar template engine
+
+    .set('views', path.join(__dirname, "view"))
+    .set('view engine', 'hbs')
+
     // criar uma rota 
 
     .get('/', (request, response) => {
-        return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+        return response.render('index')
 
     })
 
